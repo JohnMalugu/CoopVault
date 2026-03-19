87cec7f9e1ffc3305d6b3cf32984d9a1b0ca0039
JohnMalugu <johnmalugu99@gmail.com>
2026-03-20 00:06:13 +0300

    minpr

--- src/modules/savings/SavingsPage.tsx
+++ src/modules/savings/SavingsPage.tsx
@@ -26,6 +26,9 @@ export const SavingsPage: React.FC = () => {
           <Button variant="secondary" size="sm" className="gap-2">
               <Download size={16} /> Export
             </Button>
+            <Button size="sm" className="gap-2" onClick={() => toast.success('Opening new contribution form...')}>
+              <Plus size={16} /> New Contribution
+            </Button>
             <Button size="sm" icon="+" onClick={() => toast.success('Opening new contribution form...')}>
               New Contribution
             </Button>
