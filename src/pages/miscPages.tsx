9e2e88de34aa0e19a9f84dc76ec35ceb8e24bfeb
JohnMalugu <johnmalugu99@gmail.com>
2026-04-03 12:34:41 +0300

    yep

--- src/pages/miscPages.tsx
+++ src/pages/miscPages.tsx
@@ -127,7 +127,7 @@ export const LedgerPage: React.FC = () => {
           <>
           //hapa
             <Button variant="secondary" size="sm">{} Export PDF</Button>
-            <Button variant="secondary" size="sm">{} Export Excel</Button>
+            <Button variant="secondary" size="sm"> Export Excel</Button>
           </>
         }
       />
