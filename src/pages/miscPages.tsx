24bf5ed333319f0d655daa45486b4a24f3db707f
JohnMalugu <johnmalugu99@gmail.com>
2026-04-07 09:44:07 +0300

    share

--- src/pages/miscPages.tsx
+++ src/pages/miscPages.tsx
@@ -127,7 +127,7 @@ export const LedgerPage: React.FC = () => {
           <>
           //test cmnt
             <Button variant="secondary" size="sm">{Share} Export PDF</Button>
-            <Button variant="secondary" size="sm">{} Export Excel</Button>
+            <Button variant="secondary" size="sm">{Share} Export Excel</Button>
           </>
         }
       />
