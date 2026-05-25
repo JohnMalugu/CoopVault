ab9a7092f9849a9a023ad4394d92c06a73808c26
JohnMalugu <johnmalugu99@gmail.com>
2026-05-25 09:01:01 +0300

    buttons

--- src/pages/miscPages.tsx
+++ src/pages/miscPages.tsx
@@ -133,6 +133,7 @@ export const LedgerPage: React.FC = () => {
             <Button variant="secondary" size="sm">📊 Export MN Excel</Button>
             <Button variant="secondary" size="sm">📊 Export MN Excel</Button>
             <Button variant="secondary" size="sm">📊 Export MN Excel</Button>
+            <Button variant="secondary" size="sm">📊 Export MN Excel</Button>
 
 
 
