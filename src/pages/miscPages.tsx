7e0824d0297c5cff8073c68fcde8fe3e30269c3c
JohnMalugu <johnmalugu99@gmail.com>
2026-06-14 21:41:08 +0300

    mnx

--- src/pages/miscPages.tsx
+++ src/pages/miscPages.tsx
@@ -217,6 +217,8 @@ export const PenaltiesPage: React.FC = () => (
     <PageHeader title="Loan Penalties" subtitle="Track late payment penalties on your loans" />
     <Table
       title="Penalty Records ADD"
+      object = {object}
+      obj = {object}
       data={mockPenalties}
       total={0}
       totalPages={0}
