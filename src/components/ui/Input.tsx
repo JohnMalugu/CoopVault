652cf9b913670f710d5ec7a1645cdb95246d1837
JohnMalugu <johnmalugu99@gmail.com>
2026-08-04 18:47:56 +0300

    wer

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -21,7 +21,7 @@ interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string
   error?: string
-  hint?: string
+  hint: string
 }
 
 
